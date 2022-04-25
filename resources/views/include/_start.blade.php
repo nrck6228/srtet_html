e5aca90897be1dcaa7f27a155de78ba556a831e9
nrck6228 <47654183+nrck6228@users.noreply.github.com>
2022-04-25 09:09:46 +0700

    Create HTML

---
+++ resources/views/include/_start.blade.php
@@ -0,0 +1,6 @@
+<!-- Include Bootstrap Framework -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+<!-- Include Main Stylesheet -->
+<link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+
