b061b0cf767018d9e61b791b220c54f4e1fc6f3c
santanon <47654173+santanon@users.noreply.github.com>
2022-05-12 10:16:09 +0700

    update

--- resources/views/include/_social.blade.php
+++ resources/views/include/_social.blade.php
@@ -1,30 +1,28 @@
 <div class="social--wrapper">
-    <div class="container">
-        <div class="row row-cols-1 row-cols-md-2 justify-content-between">
-            <div class="col">
-                <div class="view--count">
-                    จำนวนผู้เข้าชม : <span>39068</span> คน
-                </div>
+    <div class="row row-cols-1 row-cols-md-2 justify-content-between">
+        <div class="col">
+            <div class="view--count">
+                จำนวนผู้เข้าชม : <span>39068</span> คน
             </div>
-            <div class="col">
-                <div class="social--block">
-                    <span>แชร์ :</span>
-                    <a href="javascript:void(0)" class="social__icon">
-                        <img src="{{ asset('assets/images/icon/facebook.svg')}}" alt="Facebook">
-                    </a>
-                    <a href="javascript:void(0)" class="social__icon">
-                        <img src="{{ asset('assets/images/icon/twitter.svg')}}" alt="Twitter">
-                    </a>
-                    <a href="javascript:void(0)" class="social__icon">
-                        <img src="{{ asset('assets/images/icon/icon-line.svg')}}" alt="Line">
-                    </a>
-                    <a href="javascript:void(0)" class="social__icon">
-                        <img src="{{ asset('assets/images/icon/icon-link.svg')}}" alt="Copy Link">
-                    </a>
-                    <a href="javascript:void(0)" class="social__icon">
-                        <img src="{{ asset('assets/images/icon/icon-print.svg')}}" alt="Print">
-                    </a>
-                </div>
+        </div>
+        <div class="col">
+            <div class="social--block">
+                <span>แชร์ :</span>
+                <a href="javascript:void(0)" class="social__icon">
+                    <img src="{{ asset('assets/images/icon/facebook.svg')}}" alt="Facebook">
+                </a>
+                <a href="javascript:void(0)" class="social__icon">
+                    <img src="{{ asset('assets/images/icon/twitter.svg')}}" alt="Twitter">
+                </a>
+                <a href="javascript:void(0)" class="social__icon">
+                    <img src="{{ asset('assets/images/icon/icon-line.svg')}}" alt="Line">
+                </a>
+                <a href="javascript:void(0)" class="social__icon">
+                    <img src="{{ asset('assets/images/icon/icon-link.svg')}}" alt="Copy Link">
+                </a>
+                <a href="javascript:void(0)" class="social__icon">
+                    <img src="{{ asset('assets/images/icon/icon-print.svg')}}" alt="Print">
+                </a>
             </div>
         </div>
     </div>
