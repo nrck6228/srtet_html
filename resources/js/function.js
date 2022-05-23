f2529fa3d9b833e6d08192811fac0b130d6da5e6
nrck6228 <47654183+nrck6228@users.noreply.github.com>
2022-05-23 11:48:57 +0700

    responsive home

--- resources/js/function.js
+++ resources/js/function.js
@@ -22,12 +22,12 @@ $(window).on('load',function(){
 
 // scroll animated logo
 $(window).on("scroll", function (t) {
-    $(this).scrollTop() > 50 ? $(".header--desktop").addClass("header--desktop__animate") : $(".header--desktop").removeClass("header--desktop__animate");
-    // if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
-    //   $(this).scrollTop() > 50 ? $(".header--mobile").addClass("header--mobile__animate") : $(".header--mobile").removeClass("header--mobile__animate");
-    // } else {
-    //     $(this).scrollTop() > 50 ? $(".header--desktop").addClass("header--desktop__animate") : $(".header--desktop").removeClass("header--desktop__animate");
-    // }
+    // $(this).scrollTop() > 50 ? $(".header--desktop").addClass("header--desktop__animate") : $(".header--desktop").removeClass("header--desktop__animate");
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+      $(this).scrollTop() > 50 ? $(".header--mobile").addClass("header--mobile__animate") : $(".header--mobile").removeClass("header--mobile__animate");
+    } else {
+        $(this).scrollTop() > 50 ? $(".header--desktop").addClass("header--desktop__animate") : $(".header--desktop").removeClass("header--desktop__animate");
+    }
 });
 
 $(".hamburger").click(function(){
