9eb3a2ce34ad33580efc54de81c81291a760dfd0
nrck6228 <47654183+nrck6228@users.noreply.github.com>
2022-06-10 15:35:55 +0700

    Add script station

---
+++ resources/views/stations/_station-01.blade.php
@@ -0,0 +1,174 @@
+<div class="row align-items-stretch">
+    <div class="col-lg-8">
+        <div class="station--content">
+            <div class="station--figure">
+                <img class="w-100" src="{{ asset('assets/images/station/bangsur.jpg')}}" alt="รถไฟฟ้าสายสีแดง">
+            </div>
+            <div class="station--data">
+                <h3 class="data__title">
+                    สถานีกลางบางซื่อ
+                </h3>
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <h3 class="data__subtitle">สิ่งอำนวยความสะดวก</h3>
+                        <div class="facilities--list">
+                            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3">
+                                <div class="col">
+                                    <a href="javascript:void(0)" class="facilities__item" data-bs-toggle="modal" data-bs-target="#facilitiesModal">
+                                        <img class="img-fluid" src="{{ asset('assets/images/facilities/connect-circle.svg')}}" alt="รถไฟฟ้าสายสีแดง">
+                                        <p class="h4">
+                                            จุดเชื่อมต่อ
+                                        </p>
+                                    </a>
+                                </div>
+
+                                <div class="col">
+                                    <a href="javascript:void(0)" class="facilities__item">
+                                        <img class="img-fluid" src="{{ asset('assets/images/facilities/escalator.svg')}}" alt="รถไฟฟ้าสายสีแดง">
+                                        <p class="h4">
+                                            บันไดเลื่อน
+                                        </p>
+                                    </a>
+                                </div>
+
+                                <div class="col">
+                                    <a href="javascript:void(0)" class="facilities__item">
+                                        <img class="img-fluid" src="{{ asset('assets/images/facilities/elevator.svg')}}" alt="รถไฟฟ้าสายสีแดง">
+                                        <p class="h4">
+                                            ลิฟต์
+                                        </p>
+                                    </a>
+                                </div>
+
+                                <div class="col">
+                                    <a href="javascript:void(0)" class="facilities__item">
+                                        <img class="img-fluid" src="{{ asset('assets/images/facilities/atm-machine.svg')}}" alt="รถไฟฟ้าสายสีแดง">
+                                        <p class="h4">
+                                            ATM
+                                        </p>
+                                    </a>
+                                </div>
+
+                                <div class="col">
+                                    <a href="javascript:void(0)" class="facilities__item">
+                                        <img class="img-fluid" src="{{ asset('assets/images/facilities/bus-station.svg')}}" alt="รถไฟฟ้าสายสีแดง">
+                                        <p class="h4">
+                                            รถประจำทาง
+                                        </p>
+                                    </a>
+                                </div>
+
+                                <div class="col">
+                                    <a href="javascript:void(0)" class="facilities__item">
+                                        <img class="img-fluid" src="{{ asset('assets/images/facilities/wc.svg')}}" alt="รถไฟฟ้าสายสีแดง">
+                                        <p class="h4">
+                                            ห้องน้ำ
+                                        </p>
+                                    </a>
+                                </div>
+
+                                <!-- <div class="col">
+                                    <a href="javascript:void(0)" class="facilities__item">
+                                        <img class="img-fluid" src="{{ asset('assets/images/facilities/store-alt.svg')}}" alt="รถไฟฟ้าสายสีแดง">
+                                        <p class="h4">
+                                            ร้านค้า
+                                        </p>
+                                    </a>
+                                </div> -->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="text--editor">
+                            <div class="ck-content">
+                                <h3>รายละเอียดทางเข้า - ออก</h3>
+                                <ul>
+                                    <li>ประตู 1 : สถานีขนส่ง บขส. , MRT บางซื่อ</li>
+                                    <li>ประตู 2 : SCG สำนักงานใหญ่</li>
+                                    <li>ประตู 3 : BTS จตุจักร , ตลาดนัดจตุจักร</li>
+                                    <li>ประตู 1 : สถานีขนส่ง บขส. , MRT บางซื่อ</li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <a href="/bangsur-station" title="รายละเอียดเพิ่มเติม" class="btn btn--primary">
+                            <span>รายละเอียดเพิ่มเติม</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-4">
+        <div class="fare--wrapper">
+            <h3 class="h2">
+                ค่าโดยสาร
+            </h3>
+
+            <div class="fare--rate">
+                <div class="train--line">
+                    <h3>สายบางซื่อ - ตลิ่งชัน /ราคา : </h3>
+                    <div class="fare__item">
+                        <span class="name">บางช่อน</span>
+                        <span class="price">18 บาท</span>
+                    </div>
+
+                    <div class="fare__item">
+                        <span class="name">บางบำหรุ</span>
+                        <span class="price">29 บาท</span>
+                    </div>
+
+                    <div class="fare__item">
+                        <span class="name">ตลิ่งชัน</span>
+                        <span class="price">35 บาท</span>
+                    </div>
+                </div>
+
+                <div class="train--line">
+                    <h3>สายบางซื่อ - รังสิต /ราคา : </h3>
+                    <div class="fare__item">
+                        <span class="name">จตุจักร</span>
+                        <span class="price">16 บาท</span>
+                    </div>
+
+                    <div class="fare__item">
+                        <span class="name">วัดเสมียนนารี</span>
+                        <span class="price">19 บาท</span>
+                    </div>
+
+                    <div class="fare__item">
+                        <span class="name">บางเขน</span>
+                        <span class="price">20 บาท</span>
+                    </div>
+
+                    <div class="fare__item">
+                        <span class="name">ทุ่งสองห้อง</span>
+                        <span class="price">23 บาท</span>
+                    </div>
+
+                    <div class="fare__item">
+                        <span class="name">จตุจักร</span>
+                        <span class="price">16 บาท</span>
+                    </div>
+
+                    <div class="fare__item">
+                        <span class="name">วัดเสมียนนารี</span>
+                        <span class="price">19 บาท</span>
+                    </div>
+
+                    <div class="fare__item">
+                        <span class="name">บางเขน</span>
+                        <span class="price">20 บาท</span>
+                    </div>
+
+                    <div class="fare__item">
+                        <span class="name">ทุ่งสองห้อง</span>
+                        <span class="price">23 บาท</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
